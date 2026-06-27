08988d9ddaeb2dad673edf39ae7adb73df30af4a
WARPIE\warpi <warpie_7@hotmail.com>
2026-06-26 20:14:23 -0600

    updated the documentation

--- src/augmentation.py
+++ src/augmentation.py
@@ -1,3 +1,26 @@
+
+"""
+Script: augmentation.py
+Description: Seen the low quantity of samples for training (ultrasound images), the batch was increased by applying
+             data augmentation. This script reads a folder with samples and then applies data augmentaion by transformig
+             the images with rotation, scaling, blur, saturation
+
+Author: Manuel Huitrado
+Date: June 2026 (originaly created in 2024)
+Versión: 1.0.0
+Licence: MIT License (as origanly created by eriklindernoren )
+
+Dependencies:
+- opencv
+- os
+- time
+- imgaug
+"""
+
+
+
+
+
 1 import os
 2 import cv2
 3 import imgaug as ia
@@ -26,32 +49,32 @@
 26    ], random_order=True)
 27
 28    for i in range(num_augmentations):
-29        # Aplica la secuencia de aumentación aleatoria a la imagen original
+29        # random transformation
 30        augmented_image = seq.augment_image(original_image)
 31
-32        # Guarda la imagen aumentada
+32        # stores the image with following format     filename_aug_nameofile.png
 33        output_path = os.path.join(output_folder, f"{file_name}_aug_{i}{file_extension}")
 34        cv2.imwrite(output_path, augmented_image)
 35
 36def iterate_over_images(directory_path, output_folder, num_augmentations=2):
-37    # Itera sobre archivos en el directorio
+37    # iteration over folder
 38    for filename in os.listdir(directory_path):
 39        if filename.endswith(".jpg") or filename.endswith(".png"):
-40            # Construye la ruta completa de la imagen
+40            #
 41            image_path = os.path.join(directory_path, filename)
 42
-43            # Aplica data augmentation a la imagen
+43            # data augmentation
 44            apply_random_data_augmentation(image_path, output_folder, num_augmentations)
 45
 46if __name__ == "__main__":
-47    # Ruta del directorio de imágenes originales
+47    # path for original images
 48    input_directory = "./Butte/"
 49
-50    # Carpeta de salida para las imágenes aumentadas
+50    # path for output
 51    output_folder = "./Output/"
 52
-53    # Número de aumentaciones a aplicar
+53    # types of transformations to be applied
 54    num_augmentations = 2
 55
-56    # Itera sobre imágenes y aplica data augmentation
+56    # iteration function
 57    iterate_over_images(input_directory, output_folder, num_augmentations)
